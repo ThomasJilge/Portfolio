fe1bce0529d8468e08db823dce33c2a1007fc1f0
ThomasJilge <thomasjilge@gmx.de>
2024-09-01 21:07:21 +0200

    html + scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FirstSectionAboutMeComponent } from './first-section-about-me/first-section-about-me.component';
+import { WhyMeComponent } from './why-me/why-me.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FirstSectionAboutMeComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FirstSectionAboutMeComponent, WhyMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
