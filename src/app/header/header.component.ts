ddaaf3ac5d4abc6f119aad113fc619d645320724
ThomasJilge <thomasjilge@gmx.de>
2024-09-19 21:20:17 +0200

    onWindowScroll added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -20,6 +20,23 @@ export class HeaderComponent {
 
   setLanguage(language: string) {
     this.selectedLanguage = language;
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const sections = ['whyMe', 'mySkills', 'myProjects', 'contactForm'];
+    
+    for (const section of sections) {
+      const element = document.getElementById(section);
+      if (element) {
+        const bounding = element.getBoundingClientRect();
+        
+        if (bounding.top >= 0 && bounding.top <= window.innerHeight / 2) {
+          this.selectedSection = section;
+          break;
+        }
+      }
+    }
   }
 
 }
