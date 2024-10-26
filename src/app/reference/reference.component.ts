fbb72331e9fdbea9d75025ed02fce972a5cd106e
ThomasJilge <thomasjilge@gmx.de>
2024-10-26 21:04:08 +0200

    my projects events added

--- src/app/reference/reference.component.ts
+++ src/app/reference/reference.component.ts
@@ -5,8 +5,34 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './reference.component.html',
-  styleUrl: './reference.component.scss'
+  styleUrls: ['./reference.component.scss'],
 })
 export class ReferenceComponent {
+  isDragging = false;
+  startX = 0;
+  initialPosition = 0;
+  currentTransform = 0;
 
+  onMouseDown(event: MouseEvent) {
+    this.isDragging = true;
+    this.startX = event.clientX;
+    this.initialPosition = this.currentTransform;
+    event.preventDefault();
+  }
+
+
+  onMouseMove(event: MouseEvent) {
+    if (this.isDragging) {
+      const deltaX = event.clientX - this.startX; 
+      this.currentTransform = this.initialPosition + deltaX; 
+      const container = event.currentTarget as HTMLElement; 
+      container.style.transform = `translateX(${this.currentTransform}px)`; 
+    }
+  }
+
+  onMouseUp(event: MouseEvent) {
+    this.isDragging = false;
+  }
 }
+
+
