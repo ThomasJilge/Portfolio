a550558979e91d601805beed1eeb94ed052224f3
ThomasJilge <thomasjilge@gmx.de>
2025-02-02 15:22:06 +0100

    imprint added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,8 @@ import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './main-content/header/header.component';
-import { FirstSectionAboutMeComponent } from './main-content/first-section-about-me/first-section-about-me.component';
-import { WhyMeComponent } from './main-content/why-me/why-me.component';
-import { MySkillsComponent } from './main-content/my-skills/my-skills.component';
-import { MyProjectsComponent } from './main-content/my-projects/my-projects.component';
-import { ReferenceComponent } from './main-content/reference/reference.component';
 import { FooterComponent } from './main-content/footer/footer.component';
-import { ContactformComponent } from "./main-content/contactform/contactform.component";
-import { MainContentComponent } from "./main-content/main-content.component";
+
 
 @Component({
     selector: 'app-root',
@@ -19,14 +13,8 @@ import { MainContentComponent } from "./main-content/main-content.component";
   imports: [CommonModule,
     RouterOutlet,
     HeaderComponent,
-    FirstSectionAboutMeComponent,
-    WhyMeComponent,
-    MySkillsComponent,
-    MyProjectsComponent,
-    ReferenceComponent,
-    FooterComponent,
-    ContactformComponent, 
-    MainContentComponent],
+    FooterComponent
+  ]
 })
 
 export class AppComponent {
