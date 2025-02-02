9349481f33e6b05e94445eb2cc770bd8a901a8ec
ThomasJilge <thomasjilge@gmx.de>
2025-02-02 14:50:33 +0100

    mainComponent fixed

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { FirstSectionAboutMeComponent } from "./first-section-about-me/first-section-about-me.component";
+import { WhyMeComponent } from "./why-me/why-me.component";
+import { MySkillsComponent } from "./my-skills/my-skills.component";
+import { MyProjectsComponent } from "./my-projects/my-projects.component";
+import { ReferenceComponent } from "./reference/reference.component";
+import { ContactformComponent } from "./contactform/contactform.component";
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [],
+  imports: [FirstSectionAboutMeComponent, WhyMeComponent, MySkillsComponent, MyProjectsComponent, ReferenceComponent, ContactformComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
