f5f3453d7eb0f4340c18984995f7941619797e03
ThomasJilge <thomasjilge@gmx.de>
2025-02-02 16:00:03 +0100

    imprint updated

--- src/app/main-content/imprint/imprint.component.ts
+++ src/app/main-content/imprint/imprint.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
 export class ImprintComponent {
+
+  constructor(private translate: TranslateService) { 
+    translate.setDefaultLang('en');
+    translate.use('en');
+  }
 
 }
