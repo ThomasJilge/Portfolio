634c4b906beb0f927127c0d8812576d5dd6ca9fc
ThomasJilge <thomasjilge@gmx.de>
2025-04-04 17:52:26 +0200

    scrollToTop added

--- src/app/main-content/data-protection/data-protection.component.ts
+++ src/app/main-content/data-protection/data-protection.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit  } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateService, TranslateModule } from '@ngx-translate/core';
 
@@ -9,11 +9,19 @@ import { TranslateService, TranslateModule } from '@ngx-translate/core';
   templateUrl: './data-protection.component.html',
   styleUrl: './data-protection.component.scss'
 })
-export class DataProtectionComponent {
+export class DataProtectionComponent implements AfterViewInit {
 
   constructor(private translate: TranslateService) { 
     translate.setDefaultLang('en');
     translate.use('en');
+  }
+
+  ngAfterViewInit() {
+    this.scrollToTop();
+  }
+
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   }
 
 }
