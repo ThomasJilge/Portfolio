7551a12f2fe9a9e735d537a85fe9d823fedeff7c
ThomasJilge <thomasjilge@gmx.de>
2025-08-07 18:50:43 +0200

    my skills imgs + text updated

--- src/app/main-content/my-skills/my-skills.component.ts
+++ src/app/main-content/my-skills/my-skills.component.ts
@@ -16,17 +16,17 @@ export class MySkillsComponent {
     translate.use('en');
   }
 
-  skillImages: string[] = [
-    '../../assets/img/skills/angular.png',
-    '../../assets/img/skills/typescript.png',
-    '../../assets/img/skills/javascript.png',
-    '../../assets/img/skills/html.png',
-    '../../assets/img/skills/css.png',
-    '../../assets/img/skills/rest.api.png',
-    '../../assets/img/skills/firebase.png',
-    '../../assets/img/skills/git.png',
-    '../../assets/img/skills/scrum.png',
-    '../../assets/img/skills/materialDesign.png',
-    '../../assets/img/skills/challengeMe.png'
+skillImages: { src: string; label: string }[] = [
+  { src: '../../assets/img/skills/angularNew.png', label: 'Angular' },
+  { src: '../../assets/img/skills/typescriptNew.png', label: 'TypeScript' },
+  { src: '../../assets/img/skills/javascriptNew.png', label: 'JavaScript' },
+  { src: '../../assets/img/skills/htmlNew.png', label: 'HTML' },
+  { src: '../../assets/img/skills/cssNew.png', label: 'CSS' },
+  { src: '../../assets/img/skills/rest.api.new.png', label: 'REST-API' },
+  { src: '../../assets/img/skills/firebaseNew.png', label: 'Firebase' },
+  { src: '../../assets/img/skills/gitNew.png', label: 'Git' },
+  { src: '../../assets/img/skills/scrumNew.png', label: 'Scrum' },
+  { src: '../../assets/img/skills/materialDesignNew.png', label: 'Material Design' },
+  { src: '../../assets/img/skills/challengeMeNew.png', label: 'Challenge Me' }
   ];
 }
